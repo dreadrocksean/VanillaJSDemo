ecda02fc2f528ec00ef46deb485512ad4ca43fab
Adrian Bartholomew <adrian@bartholomusic.com>
2018-01-05 17:17:07 -0600

    update main margin top on render.

---
+++ js/templates/contents.js
@@ -0,0 +1,62 @@
+var mainContents = {
+	main1: '<div> \
+		<div class="listPositionNum"> \
+            <span>1</span> \
+        </div> \
+		<blockquote> \
+			<a href="#"> \
+				The best and most beautiful things in the world cannot be seen or even touched - they must be felt with the heart. \
+			</a> \
+			<cite> \
+				<a href="#"> \
+					Helen Keller \
+				</a> \
+			</cite> \
+		</blockquote> \
+	</div>',
+	main2: '<div> \
+		<div class="listPositionNum"> \
+            <span>2</span> \
+        </div> \
+		<blockquote> \
+			<a href="#"> \
+				It is during our darkest moments that we must focus to see the light. \
+			</a> \
+			<cite> \
+				<a href="#"> \
+					Aristotle \
+				</a> \
+			</cite> \
+		</blockquote> \
+	</div>',
+	main3: '<div> \
+		<div class="listPositionNum"> \
+            <span>3</span> \
+        </div> \
+		<blockquote> \
+			<a href="#"> \
+				Find a place inside where there\'s joy, and the joy will burn out the pain. \
+			</a> \
+			<cite> \
+				<a href="#"> \
+					Joseph Campbell \
+				</a> \
+			</cite> \
+		</blockquote> \
+	</div>',
+	main4: '<div> \
+		<div class="listPositionNum"> \
+            <span>4</span> \
+        </div> \
+		<blockquote> \
+			<a href="#"> \
+				Nothing is impossible, the word itself says \'I\'m possible\'! \
+			</a> \
+			<cite> \
+				<a href="#"> \
+					Audrey Hepburn \
+				</a> \
+			</cite> \
+		</blockquote> \
+	</div>',
+};
